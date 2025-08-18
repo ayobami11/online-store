9e0d13556694237bde1814fcb086a8c06126c3aa
Ayobami Tunwase <tunwaseayobami98@gmail.com>
2025-08-18 11:04:42 +0100

    Fixed Vercel deployment errors.

--- src/app/dashboard/payment/page.tsx
+++ src/app/dashboard/payment/page.tsx
@@ -1,37 +1,10 @@
 import Link from "next/link";
-import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
 
 import { ArrowLeft } from "lucide-react";
 
-export const TransactionRow = () => {
-  return (
-    <div className="flex justify-between gap-4">
-      <div className="flex gap-2 items-center">
-        <Image
-          src="/assets/images/withdrawal-icon.png"
-          alt=""
-          width={30}
-          height={30}
-        />
-        <span>Withdrawal</span>
-      </div>
-      <div className="flex flex-col gap-1">
-        <span className="text-[#8D8D8D] text-xs">Date</span>
-        <span className="text-sm">21 July</span>
-      </div>
-      <div className="flex flex-col gap-1">
-        <span className="text-[#8D8D8D] text-xs">Time</span>
-        <span className="text-sm">09:00</span>
-      </div>
-      <div className="flex flex-col gap-1">
-        <span className="text-[#8D8D8D] text-xs">Amount</span>
-        <span className="text-sm">₦ 500</span>
-      </div>
-    </div>
-  );
-};
+import { TransactionRow } from "@/app/dashboard/transaction-row";
 
 export default function PaymentPage() {
   return (
--- src/app/dashboard/recent-transactions.tsx
+++ src/app/dashboard/recent-transactions.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 
-import { TransactionRow } from "@/app/dashboard/payment/page";
+import { TransactionRow } from "@/app/dashboard/transaction-row";
 
 export const RecentTransactions = () => {
   return (
